c884b5edcd438e36f18c0d53006450e4a6321b81
Mateusz Herych <heniekk@gmail.com>
2020-04-04 04:26:37 +0100

    Write to DOM

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
-fun main(args: Array<String>) {
-    println("Hello!")
+import org.w3c.dom.HTMLDivElement
+import kotlin.browser.document
+import kotlin.dom.appendText
+
+fun main() {
+    val content = document.getElementById("content") as HTMLDivElement
+    content.appendText("Hello from Kotlin!")
 }
